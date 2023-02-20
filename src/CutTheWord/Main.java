3ec6c621838a1bede47fa214288b8b44aecad4b7
Bread950 <banhmi2390@gmail.com>
2023-02-21 02:08:33 +0700

    New change in Demo3

--- src/CutTheWord/Main.java
+++ src/CutTheWord/Main.java
@@ -19,6 +19,6 @@ public class Main {
 		System.out.println("Bike");
 		System.out.println("Branch4");
 		System.out.println("Demo3");
-		System.out.println("New Patch");
+		System.out.println("New Patch2");
 	}
 }
